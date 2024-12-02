f26b96f002aa31008cc83bd66629b00283f2bdd0
Bruno César <Bc97855@gmail.com>
2024-12-02 00:15:34 -0300

    Criacao de tabela de personagens

--- Business/play.js
+++ Business/play.js
@@ -6,10 +6,41 @@ document.getElementById('backButton').addEventListener('click', () => {
 });
 
 document.getElementById('confirm').addEventListener('click', () => {
-  const name = document.getElementById('nameCaracter')
-  const character = [
-    { name: name.value, class: 'classeTeste', level: 1 },
-  ];
-  addPersonagem(character);
+  const saves = getPersonagens();
+  if(saves.length < 5){
+    const name = document.getElementById('nameCaracter')
+    const character = { name: name.value, class: 'classeTeste', level: 1 };
+    addPersonagem(character);
+  }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadSaves();
+});
+
+function loadSaves() {
+  const saves = getPersonagens();
+  const tableBody = document.querySelector('#table tbody');
+
+  tableBody.innerHTML = '';
+
+  if (saves.length === 0) {
+    tableBody.innerHTML = '<tr><td colspan="3">No saves found</td></tr>';
+    return;
+  }
+
+  saves.forEach(x => {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${x.name}</td>
+      <td>${x.level}.lv</td>
+    `;
+
+    tableBody.appendChild(row);
+  });
+
+  const header = document.querySelector('#table thead th');
+  header.textContent = `${saves.length}/5`;
+}
   
